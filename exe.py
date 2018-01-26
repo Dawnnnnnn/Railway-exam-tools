0f8e8bc449d1cc22286cc0ad8bbfbe6bd6cf7e12
Dawnspace <1050596704@qq.com>
2018-01-26 22:40:03 +0800

    finished

--- exe.py
+++ exe.py
@@ -1,5 +1,6 @@
 import win32gui
 import time
+import xlrd
 import win32api
 import win32con
 
@@ -14,13 +15,13 @@ try:
 except:
     print("未检测到考试窗口，请重启考试系统或联系作者")
     exit(-2)
+hwndChildList = []
+win32gui.EnumChildWindows(hwnd, lambda hwnd, param: param.append(hwnd),  hwndChildList)
+
+
+#经观察“首 题”的下一个元素为题目
 while 1:
     try:
-        hwndChildList = []
-        win32gui.EnumChildWindows(hwnd, lambda hwnd, param: param.append(hwnd),  hwndChildList)
-        #print(hwndChildList)
-        #列表第十一/十三个为题目，取其标题
-        #经观察“首 题”的下一个元素为题目
         for i in range(0,len(hwndChildList)):
             title = win32gui.GetWindowText(hwndChildList[i])
             if title == "首 题":
@@ -28,22 +29,65 @@ while 1:
                 num = temp.split("、",1)[0]
                 question = temp.split("、",1)[1]
                 print(str(num)+"、"+question)
-                #“错”的位置
-                #win32api.SetCursorPos([116, 424])
-                #“对”的位置
-                #win32api.SetCursorPos([116,324])
-                #“下题"的位置
-                #win32api.SetCursorPos([366,524])
-                #"D"的位置
-                #win32api.SetCursorPos([116,444])
-                #"C"的位置
-                #win32api.SetCursorPos([116,394])
-                #"B"的位置
-                #win32api.SetCursorPos([116,344])
-                #"A"的位置
-                win32api.SetCursorPos([116,294])
-                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-        time.sleep(100)
+                data = xlrd.open_workbook('exercise.xls')
+                table = data.sheets()[0]
+                nrows = table.nrows
+                lenOfXls = len(data.sheets())
+                questionlist = []
+                for x in range(0, lenOfXls):
+                    xls = data.sheets()[x]
+                    for i in range(1, nrows):
+                        temp = xls.cell(i,1).value
+                        questionlist.append(temp)
+                if question in questionlist:
+                    num = [i for i, x in enumerate(questionlist) if x == question]
+                    answer = xls.cell(num[0] + 1, 7).value
+                    list1 = []
+                    for item in range(0, len(answer), 1):
+                        temp = str(answer[item:item + 1])
+                        list1.append(temp)
+                    if "A" in list1:
+                        print(xls.cell(num[0] + 1, 3).value)
+                    if "B" in list1:
+                        print(xls.cell(num[0] + 1, 4).value)
+                    if "C" in list1:
+                        print(xls.cell(num[0] + 1, 5).value)
+                    if "D" in list1:
+                        print(xls.cell(num[0] + 1, 6).value)
+
+                if question not in questionlist:
+                    question = question.replace(",","，")
+                    if question in questionlist:
+                        num = [i for i, x in enumerate(questionlist) if x == question]
+                        answer = xls.cell(num[0]+1,7).value
+                        list1 = []
+                        for item in range(0, len(answer), 1):
+                            temp = str(answer[item:item + 1])
+                            list1.append(temp)
+                        if "A" in list1:
+                            print(xls.cell(num[0]+1, 3).value)
+                        if "B" in list1:
+                            print(xls.cell(num[0]+1, 4).value)
+                        if "C" in list1:
+                            print(xls.cell(num[0]+1, 5).value)
+                        if "D" in list1:
+                            print(xls.cell(num[0]+1, 6).value)
+
+                        #“错”的位置
+                        #win32api.SetCursorPos([116, 424])
+                        #“对”的位置
+                        #win32api.SetCursorPos([116,324])
+                        #“下题"的位置
+                        #win32api.SetCursorPos([366,524])
+                        #"D"的位置
+                        #win32api.SetCursorPos([116,444])
+                        #"C"的位置
+                        #win32api.SetCursorPos([116,394])
+                        #"B"的位置
+                        #win32api.SetCursorPos([116,344])
+                        #"A"的位置
+                        #win32api.SetCursorPos([116,294])
+                        #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(3)
     except:
-        print("未检测到考试窗口，请重启考试系统或联系作者")
-        exit(-1)
+        pass
